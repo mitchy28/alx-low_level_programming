2d9193e0d6fe6371fe7487c54970602996ac0887
mitchy28 <mitchelchipungu28@gmail.com>
2022-09-12 12:48:34 -0700

    Inventing is a combination of brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
-
 /**
- * main - print all possible different combination of two digits
- *
- * Description:This program print only the smallest combination of the two digits
- * 
- * Return Always 0 (Success)
+ * main - program that prints all possible different combinations of 2 digits
+ * Return: 0
  */
 int main(void)
 {
-	int i= 0; i = 10
-		putchar("01, 02\n");
-		putchar("03, 04\n");
-		putchar("05, 06\n");
-		putchar("07, 08\n");
-		putchar("09, 10\n");
-		return (0);
-}
+	int c = 0;
+	int f_d;
+	int l_d;
 
+	while (c <= 99)
+	{
+		f_d = (c / 10 + '0');
+		l_d = (c % 10 + '0');
+
+		if (f_d < l_d)
+		{
+			putchar(f_d);
+			putchar(l_d);
+
+			if (c != 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		c++;
+	}
+putchar('\n');
+return (0);
+}
