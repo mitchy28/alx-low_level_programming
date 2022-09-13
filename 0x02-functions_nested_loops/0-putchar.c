e038e636d2e8e17f2b88683b6daaf530c88be297
mitchy28 <mitchelchipungu28@gmail.com>
2022-09-13 09:11:11 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/** main - prints _putchar, followed by a new line
+ *
+ * Description: A program that prints _putchar , followed by a new line'
+ *
+ * Return Always 0 (success)
+ */
+int main(void)
+{
+	int li;
+	putchar("li\n");
+	return (0);
+}
+
