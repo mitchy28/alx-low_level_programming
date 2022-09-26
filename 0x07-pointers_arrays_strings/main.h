41c467a01dd89d4e01eec8f170bf5b9abc9ffcbd
mitchy28 <mitchelchipungu28@gmail.com>
2022-09-26 07:18:07 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@
  * int prototypes()
  */
 
-Int _putchar(char c);
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
