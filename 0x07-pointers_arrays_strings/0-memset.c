d94a1407e650c4b665dadeb4c65b61c2ae69ddb1
mitchy28 <mitchelchipungu28@gmail.com>
2022-09-26 06:52:29 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-*_memset - Entry point
-*@s: pointed destination
-*@b: constant byte
-*@n: bytes
-*Return: Always 0 (Success)
-*/
+ * _memset - Entry point
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: Always 0 (Success)
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
