6f353e182fbf3826772d659df0d6f648e6cbdaf6
mitchy28 <mitchelchipungu28@gmail.com>
2022-09-26 06:48:45 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * void prototypes()
+ * int prototypes()
+ */
+
 Int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -12,4 +17,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#endif /* MAIN_H */
+#endif /*MAIN_H*/
