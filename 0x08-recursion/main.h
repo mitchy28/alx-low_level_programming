21f48101908a2582c03e2deee7dc40018d564dae
mitchy28 <mitchelchipungu28@gmail.com>
2022-09-28 08:22:10 -0700

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,12 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
-* void prototypes
-* int prototypes
-*/
+#include <stdio.h>
 
-int _putchar(char c)
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
