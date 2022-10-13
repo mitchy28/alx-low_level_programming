0f6bc7b5644b53573a4afaba3c8b437cc554d679
mitchy28 <mitchelchipungu28@gmail.com>
2022-10-13 13:07:31 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 /**
- * print_numbers - Entry Point
- * @separator: comma space
- * @n: elements to be printed
- * Return: void
- */
+* print_numbers - Entry Point
+* @separator: comma space
+* @n: elements to be printed
+* Return: void
+*/
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
